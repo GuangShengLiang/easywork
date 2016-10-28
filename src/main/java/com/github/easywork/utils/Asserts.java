00543a6e979ce30e0c9f3b74a3525c6efcff4f01
liangguangsheng <liangguangsheng@bilibili.com>
2016-10-28 16:00:01 +0800

    save

--- src/main/java/com/github/easywork/utils/Asserts.java
+++ src/main/java/com/github/easywork/utils/Asserts.java
@@ -12,43 +12,55 @@ public class Asserts {
 
     /**
      * if reference is null then throw BizException(msg)
+     *
      * @param ref
      * @param template
      */
-    public static void notNull(Object ref, String template, Object ... args){
-        if (ref == null){
-            throw new BizException(String.format(template,args));
+    public static void notNull(final Object ref, final int code, final String template, Object... args) {
+        if (ref == null) {
+            throw new BizException(code, String.format(template, args));
         }
     }
 
-    public static void notNull(Object ref, String field){
-        notNull(ref,"%s can't be null",field);
+    public static void notNullMsg(final Object ref, final String field) {
+        notNull(ref, -1, "%s can't be null", field);
     }
-    public static void notNullMsg(Object ref, String msg){
-        if (ref == null){
-            throw new BizException(msg);
-        }
+
+    public static void notNullMsg(final Object ref, final int code, final String field) {
+        notNull(ref, code, "%s can't be null", field);
     }
+
+    public static void notNull(final Object ref, final String template, Object... args) {
+        notNull(ref, -1, template, args);
+    }
+
     /**
      * if collection is empty then throw BizException(msg)
+     *
      * @param collection
-     * @param msg
      */
-    public static void notEmpty(Collection collection, String msg){
-        if (collection == null || collection.isEmpty()){
-            throw new BizException(msg);
+    public static void notEmpty(final Collection collection, final String template, Object... args) {
+        if (collection == null || collection.isEmpty()) {
+            throw new BizException(String.format(template, args));
         }
     }
 
-    public static void isTrue(boolean expression, String template, Object... args){
-        if (expression){
-            throw new BizException(String.format(template,args));
+    public static void notEmpty(final Collection collection, final int code, final String template, Object... args) {
+        if (collection == null || collection.isEmpty()) {
+            throw new BizException(code, String.format(template, args));
         }
     }
 
-    public static void isTrue(boolean expression, String msg){
-        if (expression){
-            throw new BizException(msg);
+    public static void isTrue(final boolean expression, final String template, Object... args) {
+        if (expression) {
+            throw new BizException(String.format(template, args));
         }
     }
+
+    public static void isTrue(final boolean expression, final int code, final String template, Object... args) {
+        if (expression) {
+            throw new BizException(code, String.format(template, args));
+        }
+    }
+
 }
