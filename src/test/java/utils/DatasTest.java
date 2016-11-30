b627a230d6b7c3877d2f8f7b13da65568be70150
liangguangsheng <liangguangsheng@bilibili.com>
2016-11-30 18:55:57 +0800

    save

--- src/test/java/utils/DatasTest.java
+++ src/test/java/utils/DatasTest.java
@@ -7,6 +7,7 @@ import com.google.common.collect.Maps;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import model.Person;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.LinkedHashSet;
@@ -31,6 +32,59 @@ public class DatasTest {
         Datas.single(Lists.newLinkedList());
         Datas.single(new LinkedHashSet<>());
         Datas.single(Lists.newArrayList());
-        String.format("%%s%","s");
+//        String.format("%%s%","s");
+    }
+
+    @Test
+    public void auth(){
+//        只读
+        Assert.assertEquals(1%1 == 0,true);
+        Assert.assertEquals(1%2 == 0,false);
+        Assert.assertEquals(1%3 == 0,false);
+        Assert.assertEquals(1%5 == 0,false);
+//创建
+        Assert.assertEquals(2%1 == 0,true);
+        Assert.assertEquals(2%2 == 0,true);
+        Assert.assertEquals(2%3 == 0,false);
+        Assert.assertEquals(2%5 == 0,false);
+//只改
+        Assert.assertEquals(3%1 == 0,true);
+        Assert.assertEquals(3%2 == 0,false);
+        Assert.assertEquals(3%3 == 0,true);
+        Assert.assertEquals(3%5 == 0,false);
+//只删
+
+        Assert.assertEquals(5%1 == 0,true);
+        Assert.assertEquals(5%2 == 0,false);
+        Assert.assertEquals(5%3 == 0,false);
+        Assert.assertEquals(5%5 == 0,true);
+
+        //创建修改
+
+        Assert.assertEquals(6%1 == 0,true);
+        Assert.assertEquals(6%2 == 0,true);
+        Assert.assertEquals(6%3 == 0,true);
+        Assert.assertEquals(6%5 == 0,false);
+
+        //创建删除
+
+        Assert.assertEquals(10%1 == 0,true);
+        Assert.assertEquals(10%2 == 0,true);
+        Assert.assertEquals(10%3 == 0,false);
+        Assert.assertEquals(10%5 == 0,true);
+
+        //修改删除
+
+        Assert.assertEquals(15%1 == 0,true);
+        Assert.assertEquals(15%2 == 0,false);
+        Assert.assertEquals(15%3 == 0,true);
+        Assert.assertEquals(15%5 == 0,true);
+
+        //全部
+
+        Assert.assertEquals(0%1 == 0,true);
+        Assert.assertEquals(0%2 == 0,true);
+        Assert.assertEquals(0%3 == 0,true);
+        Assert.assertEquals(0%5 == 0,true);
     }
 }
